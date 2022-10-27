166b814e221a217ce0a081f16a61584df86d8eda
unknown <m.o.mustafov@gmail.com>
2022-10-28 02:16:04 +0300

    created simple bootstrap main page layout with input for word

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button, Form, Container, Card } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* <Container> */}
+        <Container>
+
+      <Card className="w-100 m-auto border border-bottom border-0 border-5 p-2">
+        <Form>
+          <Form.Group className="d-flex flex-wrap p-3">
+            <Form.Label className="h4">Word</Form.Label>
+            <Form.Control
+              className="w-100"
+              placeholder="example"
+            ></Form.Control>
+          </Form.Group>
+        </Form>
+      </Card>
+      <Card className="w-100 mt-4 border  border-0 border-bottom h3 pb-2">
+        Meaning
+        </Card>
+        <Card className="border-0" >...</Card>
+
+        <Card className="w-100 mt-4 border  border-0 border-bottom h3 pb-2">
+        Examples
+        </Card>
+        <Card className="border-0 " >...</Card>
+
+
+
+        <Card className="w-100 mt-4 border  border-0 border-bottom h3 pb-2">
+        Synonyms
+        </Card>
+        <Card className="border-0 " >...</Card>
+
+
+</Container>
+      {/* <Container className="p-1">aaa</Container> */}
+        {/* </Container> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
