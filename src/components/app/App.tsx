eb8ad6f659a0e0c8e706e9266495820060307294
unknown <m.o.mustafov@gmail.com>
2022-11-03 12:15:31 +0200

    added routing

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 // STYLES
 import './App.scss'
@@ -18,191 +19,45 @@ import { Meaning } from '../meaning/Meaning'
 import { Spinner } from '../spinner/Spinner'
 import { NavPartOfSpeech } from '../navPartOfSpeach/NavPartOfSpeech'
 
+import { Home } from '../Home/Home'
+import { Search } from '../Search/Search'
+
 function App() {
   window.addEventListener('scroll', (e) => navInputOnScroll(e))
 
-  let key: number = 1
-
-    const [meaningImages, setMeaningImages] = useState<ImageData[]>([])
-
-
-  const [word, setWord] = useState<string>('')
-  const [wordSecondary, setWordSecondary] = useState<string>('')
-  const [loading, setLoading] = useState<boolean>(false)
-  const [error, setError] = useState('')
-
-  const [definitions, setDefinitions] = useState<Definitions>()
   const [partOfSpeech, setPartOfSpeech] = useState<string[]>([])
-
-
-  async function request(e: string) {
-    const o: Definitions = {
-      noun: { d: [], m: [], s: [], a: [], ex: [] },
-      pronoun: { d: [], m: [], s: [], a: [], ex: [] },
-      verb: { d: [], m: [], s: [], a: [], ex: [] },
-      adjective: { d: [], m: [], s: [], a: [], ex: [] },
-      adverb: { d: [], m: [], s: [], a: [], ex: [] },
-      preposition: { d: [], m: [], s: [], a: [], ex: [] },
-      conjunction: { d: [], m: [], s: [], a: [], ex: [] },
-      interjection: { d: [], m: [], s: [], a: [], ex: [] },
-    }
-    if (e === 'click'.trim() || e === 'enter'.trim()) {
-      //   e.preventDefault()
-      if (word.length > 0 && word !== wordSecondary) {
-        setLoading(true)
-        
-        try {
-          const res = await getMeaning(word)
-          let p_of_s: string[] = []
-          res.forEach((obj: any) => {
-            obj.meanings.forEach((def: any) => {
-              def.definitions.forEach((d: any) => {
-                if (def.partOfSpeech === 'noun'.trim()) {
-                  if (p_of_s.includes('noun'.trim()) === false) {
-                    p_of_s.push('noun')
-                  }
-                  o.noun.d.push(d.definition)
-                  if (d.example) {
-                    o.noun.ex!.push(d.example)
-                  }
-                }
-                if (def.partOfSpeech === 'pronoun'.trim()) {
-                  if (p_of_s.includes('pronoun'.trim()) === false) {
-                    p_of_s.push('pronoun'.trim())
-                  }
-                  o.pronoun.d.push(d.definition)
-                  if (d.example) {
-                    o.pronoun.ex!.push(d.example)
-                  }
-                }
-                if (def.partOfSpeech.trim() === 'verb'.trim()) {
-                  if (p_of_s.includes('verb'.trim()) === false) {
-                    p_of_s.push('verb'.trim())
-                  }
-                  o.verb.d.push(d.definition)
-                  if (d.example) {
-                    o.verb.ex!.push(d.example)
-                  }
-                }
-                if (def.partOfSpeech.trim() === 'adjective'.trim()) {
-                  if (p_of_s.includes('adjective'.trim()) === false) {
-                    p_of_s.push('adjective'.trim())
-                  }
-                  o.adjective.d.push(d.definition)
-                  if (d.example) {
-                    o.adjective.ex!.push(d.example)
-                  }
-                }
-                if (def.partOfSpeech.trim() === 'adverb'.trim()) {
-                  if (p_of_s.includes('adverb'.trim()) === false) {
-                    p_of_s.push('adverb'.trim())
-                  }
-                  o.adverb.d.push(d.definition)
-                  if (d.example) {
-                    o.adverb.ex!.push(d.example)
-                  }
-                }
-                if (def.partOfSpeech.trim() === 'preposition'.trim()) {
-                  if (p_of_s.includes('preposition'.trim()) === false) {
-                    p_of_s.push('preposition'.trim())
-                  }
-                  o.preposition.d.push(d.definition)
-                  if (d.example) {
-                    o.preposition.ex!.push(d.example)
-                  }
-                }
-                if (def.partOfSpeech.trim() === 'conjunction'.trim()) {
-                  if (p_of_s.includes('conjunction'.trim()) === false) {
-                    p_of_s.push('conjunction'.trim())
-                  }
-                  o.conjunction.d.push(d.definition)
-                  if (d.example) {
-                    o.conjunction.ex!.push(d.example)
-                  }
-                }
-                if (def.partOfSpeech.trim() === 'interjection'.trim()) {
-                  if (p_of_s.includes('interjection'.trim()) === false) {
-                    p_of_s.push('interjection'.trim())
-                  }
-                  o.interjection.d.push(d.definition)
-                  if (d.example) {
-                    o.interjection.ex!.push(d.example)
-                  }
-                }
-              })
-            })
-          })
-          setPartOfSpeech(p_of_s)
-          setDefinitions(o)
-          setError('')
-          setWordSecondary(word)
-          setMeaningImages([])
-          setTimeout(() => {
-            setLoading(false)
-          }, 1000)
-        } catch (err) {
-          setError('There is no such word !')
-          setPartOfSpeech([])
-          setDefinitions(undefined)
-          setMeaningImages([])
-          setTimeout(() => {
-            setLoading(false)
-          }, 1000)
-        }
-      }
-    }
-  }
-
-  function requestOnEnter(e: React.KeyboardEvent) {
-    if (e.code === 'Enter') {
-      request('enter')
-    }
-  }
+  const [searchLoading, setSearchLoading] = useState<boolean>(false)
+//   const [searchError, setSearchError] = useState<string>('')
 
   return (
     <div className="App">
-      <header>
-        <nav>
-          <div className="word">
-            <label htmlFor="wordInput">Word</label>
-            <div className="i-w">
-              <NavPartOfSpeech partOfSpeech={partOfSpeech}></NavPartOfSpeech>
-              <input
-                type="text"
-                id="wordInput"
-                placeholder="ex: sky"
-                onChange={(e) => setWord(e.target.value)}
-                onKeyDown={(e) => requestOnEnter(e)}
-              />
-              <i
-                className="fa-sharp fa-solid fa-magnifying-glass"
-                onClick={() => request('click')}
-              ></i>
-              {loading === false && error.length > 0 ? (
-                <div className="error">There is no such word !</div>
-              ) : null}
-            </div>
-          </div>
-        </nav>
-      </header>
-      <main>
-        {loading ? (
-          <Spinner></Spinner>
-        ) : (
-          <div className="w">
-            {definitions !== undefined ? (
-              <Meaning
-                key={key++}
-                partOfSpeech={partOfSpeech}
-                definitions={definitions}
-                word={wordSecondary}
-                meaningImages={meaningImages}
-                setMeaningImages={setMeaningImages}
-              ></Meaning>
-            ) : null}
-          </div>
-        )}
-      </main>
+      <Routes>
+        <Route
+          path="/*"
+          element={
+            <Search
+              partOfSpeech={partOfSpeech}
+            //   searchLoading={searchLoading}
+            //   setSearchLoading={setSearchLoading}
+            //   searchError={searchError}
+            //   setSearchError={setSearchError}
+            />
+          }
+        ></Route>
+        <Route
+          path="/home/*"
+          element={
+            <Home
+              partOfSpeech={partOfSpeech}
+              setPartOfSpeech={setPartOfSpeech}
+              searchLoading={searchLoading}
+              setSearchLoading={setSearchLoading}
+            //   searchError={searchError}
+            //   setSearchError={setSearchError}
+            />
+          }
+        ></Route>
+      </Routes>
     </div>
   )
 }
